c10caf895797ea55ef2914cff60c9c05291ace36
yenny <aldald73@naver.com>
2019-09-01 21:54:25 +0900

    Updates

---
+++ precache-manifest.4e9b9b766c9adc131918184b1785c776.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa38008911d73d2b1fcb6b4d7f79119",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "7b6c72b74a697de1d8af",
+    "url": "/Movie-App/static/css/main.2f1a1683.chunk.css"
+  },
+  {
+    "revision": "56e7ed5251963f33f1e4",
+    "url": "/Movie-App/static/js/2.29844d67.chunk.js"
+  },
+  {
+    "revision": "7b6c72b74a697de1d8af",
+    "url": "/Movie-App/static/js/main.95f73bbf.chunk.js"
+  },
+  {
+    "revision": "772375578610f7afcb27",
+    "url": "/Movie-App/static/js/runtime~main.2a48d573.js"
+  }
+]);
